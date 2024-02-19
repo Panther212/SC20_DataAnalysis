7d07c74912e37799f1d2a2cedfbe78e0e145c99c
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-19 16:11:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ doc = doc_ref.get()
 #st.write(doc.to_dict())
 
 #df = pd.DataFrame(doc.to_dict())
-st.write(doc)
+st.write(doc.to_dict())
 df = pd.DataFrame.from_dict(doc.to_dict(), orient='index')
 df = df.transpose()
 st.write(df);
