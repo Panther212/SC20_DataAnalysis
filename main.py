ad2dff5dd00abfc24647b5a9820af11f1c8d3eb1
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-24 20:42:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google.cloud import firestore
 import pandas as pd
 import joblib
 from prediction import predict
-from firestore import FieldFilter
+from google.cloud import FieldFilter
 from sklearn.metrics import accuracy_score, classification_report
 from scipy import signal
 from sklearn.preprocessing import MinMaxScaler
