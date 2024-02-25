a49b21a88a9f9b914b23a67811701a794de6543f
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-26 01:13:21 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,9 +1,9 @@
 import joblib
 
 
-def predict(m1,m2,m3):
- model1 = joblib.load("Model1.sav")
- model2 = joblib.load("Model2.sav")
- model3 = joblib.load("Model3.sav")
+def predict(m1):
+ model1 = joblib.load("Model.sav")
+ #model2 = joblib.load("Model2.sav")
+ #model3 = joblib.load("Model3.sav")
  #lf_norm_stat = joblib.load("norm_stats_clf.sav")
- return model1.predict(m1), model2.predict(m2),model3.predict(m3)
+ return model1.predict(m1)
