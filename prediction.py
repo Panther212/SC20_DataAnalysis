007ce2ad5cca5dc0b54cbdd404ef5b2d6b45633d
Md Faizan ur Rehman <88930802+Faizanur@users.noreply.github.com>
2024-03-15 16:48:11 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,8 +2,8 @@ import joblib
 
 
 def predict(m1,m2):
- model1 = joblib.load("model1_nodet.sav")
- model2 = joblib.load("model2_det.sav")
+ model1 = joblib.load("newmodel1.sav")
+ model2 = joblib.load("newmodel2.sav")
  #model3 = joblib.load("Model3.sav")
  #lf_norm_stat = joblib.load("norm_stats_clf.sav")
  return model1.predict(m1),model2.predict(m2)
